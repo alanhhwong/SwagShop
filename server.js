d68964af031ffa7c8cd59520fff3c7f60ebad390
Alan Hon Hei Wong <alanwong@fb.com>
2016-07-22 11:13:51 +0800

    install ssl

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-var path = require('path');
-var express = require('express');
-var routes = require('./routes');
-var bodyParser = require('body-parser');
+var path = require('path'),
+    express = require('express'),
+    routes = require('./routes'),
+    bodyParser = require('body-parser'),
+    https = require('https'),
+    fs = require('fs');
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.use('/', routes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
-app.use('/', routes);
+var options = {
+  ca: fs.readFileSync('./ssl/swagshop_alanwong_xyz.ca-bundle'),
+  key: fs.readFileSync('./ssl/swagshop_alanwong_xyz.key'),
+  cert: fs.readFileSync('./ssl/swagshop_alanwong_xyz.crt')
+};
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'), function() {
+var server = https.createServer(options, app).listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });

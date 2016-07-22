2f9e89e55796756bf6417c2e51c196300706c1d1
Alan Hon Hei Wong <alanwong@fb.com>
2016-07-22 11:39:49 +0800

    remove ssl

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var path = require('path'),
     express = require('express'),
     routes = require('./routes'),
-    bodyParser = require('body-parser'),
-    https = require('https'),
-    fs = require('fs');
+    bodyParser = require('body-parser');
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -15,12 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
-var options = {
-  ca: fs.readFileSync('./ssl/swagshop_alanwong_xyz.ca-bundle'),
-  key: fs.readFileSync('./ssl/swagshop_alanwong_xyz.key'),
-  cert: fs.readFileSync('./ssl/swagshop_alanwong_xyz.crt')
-};
-
-var server = https.createServer(options, app).listen(app.get('port'), function() {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
